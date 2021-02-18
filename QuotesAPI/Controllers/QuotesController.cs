5d414ad6c3ac3b69395d0c722e344999a422b5da
Ramya.U <ramyayadav2016@gmail.com>
2021-02-18 12:51:43 +0530

    [Ramya] Add. Added http methods

--- QuotesAPI/Controllers/QuotesController.cs
+++ QuotesAPI/Controllers/QuotesController.cs
@@ -16,7 +16,8 @@ namespace QuotesAPI.Controllers
         {
             new Quote(){Id=0,Author="Emily",Description="The brain is wider than the sky",Title="Inspirational"},
             new Quote(){Id=1, Author="Chetan Bhagath",Description="Revolution",Title="Fiction"},
-            new Quote(){Id=2,Author="Richard Bach",Description="True Love stories never have endings",Title="Love stories"}
+            new Quote(){Id=2,Author="Richard Bach",Description="True Love stories never have endings",Title="Love stories"},
+            new Quote(){Id=3,Author="Carol",Description="Only I Can change myself",Title="Fiction"}
         };
 
         [HttpGet]
@@ -26,10 +27,21 @@ namespace QuotesAPI.Controllers
         }
 
         [HttpPost]
-        public void Post(Quote quote)
+        public void Post([FromBody]Quote quote)
         {
             _quotes.Add(quote);
         }
 
+        [HttpPut("{id}")]
+        public void Put(int id,[FromBody]Quote quote)
+        {
+            _quotes[id] = quote;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _quotes.RemoveAt(id);
+        }
     }
 }
